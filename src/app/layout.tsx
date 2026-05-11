837bbdd4fdb2d8108117d85120c0d1a21a4ae029
swarajns <swarajns79@gmail.com>
2026-05-11 13:28:38 +0400

    updated gtm in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { siteConfig } from "@/lib/siteConfig";
 import Navbar from "@/components/Navbar";
@@ -40,10 +41,33 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geist.className} bg-[#0a0a0a] text-[#f5f5f5]`}>
+        {/* GTM */}
+        <Script
+          id="gtm-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P66NVQXD');`,
+          }}
+        />
+
         <Navbar />
         <main>{children}</main>
         <Footer />
         <ChatbotWrapper />
+
+        {/* GTM */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P66NVQXD"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
       </body>
     </html>
   );
